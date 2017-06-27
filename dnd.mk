44a8f5c3d52735a3b02a06734925ecfbd03f78cf
AKabhishek <abhishekkaushik.star@gmail.com>
2017-06-27 12:03:58 +0000

    kenzo: build with root
    Change-Id: Ief736625c452e21a866d8e9689318c1b18fa6d95
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- dnd.mk
+++ dnd.mk
@@ -21,6 +21,9 @@ $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 # Inherit some common AOS stuff.
 $(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
+# Magisk
+WITH_ROOT=true
+
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
